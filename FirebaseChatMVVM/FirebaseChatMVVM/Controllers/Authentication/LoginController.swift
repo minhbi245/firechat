085884a0b26337c952023a18138df79d6590f7e9
minhbi245 <minhbi245@gmail.com>
2021-01-27 16:39:17 +0700

    Add Authentication

---
+++ FirebaseChatMVVM/FirebaseChatMVVM/Controllers/Authentication/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  FirebaseChatMVVM
+//
+//  Created by KhanhNguyen on 26/01/2021.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        
+    }
+}
---
+++ FirebaseChatMVVM/FirebaseChatMVVM/Controllers/Authentication/RegistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationController.swift
+//  FirebaseChatMVVM
+//
+//  Created by KhanhNguyen on 26/01/2021.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+}
